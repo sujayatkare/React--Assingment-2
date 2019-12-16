44cb95beda04453ff14547a0ef5ca2c9a0a5394f
sujayatkare <53252801+sujayatkare@users.noreply.github.com>
2019-12-16 15:05:36 +0530

    Create App.js

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import InputField from './InputField';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <InputField />
+            </div>
+        );
+    };
+}
+
+export default App;
